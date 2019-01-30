e91087e08ad3e163a4313775ea631139169d61d2
ericstenberg <eric.d.stenberg@gmail.com>
2019-01-29 23:19:08 -0500

    RemoveElementsTests.c compiles with no warnings or errors. Iterator created. Running gdb seems to show linked list is not being created.

--- RemoveElementsTests.c
+++ RemoveElementsTests.c
@@ -7,28 +7,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct _ListItem { // define a struct _ListItem
+typedef struct _ListNode { // define a struct _ListNode
 	int data; // with int data
-	struct ListItem *next; // create pointer to next item in linked list
-} ListItem; // give it struct alias ListItem. This will be used to initialize other variables.
+	struct _ListNode *next; // create pointer to next item in linked list. When creating a struct within a struct that is of the same type, always use the struct name and not the alias.
+} ListNode; // give it struct alias ListNode. This will be used to initialize other variables.
 
-ListItem *createNode() {
-	ListItem *newNode = (ListItem *) malloc(sizeof(ListItem));
+struct ListNode* removeElementsTest(struct ListNode* head, int val) {
+
+	// Insert your code here.
+
+	return 0;
+   
+}
+
+ListNode *createNode() {
+	ListNode *newNode = (ListNode *) malloc(sizeof(ListNode));
 	newNode->data = 0;
 	newNode->next = NULL; // create pointer to next item in linked list
 	return newNode;
 }
 
-ListItem *createNextNode(ListItem *head, int val) { // create function to create notes in linked list
-	ListItem *temp, *p; // initialize temp and p as ListItem
+ListNode *createNextNode(ListNode *head, int val) { // create function to create notes in linked list
+	ListNode *temp, *p; // initialize temp and p as ListNode
 	temp = createNode();
-	temp->data = val; // set temp->data to given val
+	temp->data = val; // set temp->data to given val. temp->next has no value...yet. temp->data has an address. This address is pointed to by temp->next
+	//printf("%p\n",&(temp->data));
 	if (!head) { // if the given head is NULL
 		head = temp; // set the head to the temp location and data
 	} else { // otherwise
 		p = head; // set p equal to the head
 		while (p->next != NULL) { // iterate through linked list. If the pointer to the next item is not NULL
-			p = p->next; // set p to the next item
+			p = p->next; // set p to the next item.  Getting assignment from incompatible pointer type warning.
 		}
 		p = temp; // when the while loop is finished iterating, set p to temp, which sets p->data = val
 	}
@@ -36,19 +45,32 @@ ListItem *createNextNode(ListItem *head, int val) { // create function to create
 	return head;
 }
 
-struct ListNode* removeElementsTest(struct ListNode* head, int val) {
-	return 0;    
+void iterateNextNode(ListNode *head) {
+	ListNode *p;
+	if (!head) {
+		printf("Linked list does not exist.");
+	} else { 
+		p = head;
+		while (p->next != NULL) {
+			printf("%d\n",p->data);
+			p = p->next;
+		}
+	}
 }
 
 int main(int argc, char *argv[]) {
 
 	int i = 0, j = 10;
-	ListItem *head;
+	ListNode *head;
+
+	head = createNode();
 
 	for (i = 0; i < j; i++) {
 		head = createNextNode(head, i);
 	}
 
+	iterateNextNode(head);
+	
 	return 0;
 }
 
